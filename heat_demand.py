4e8f3667dbe934fcd44437099c8d4acd33b5a910
Rowan Molony <rowan.molony@codema.ie>
2020-12-18 15:48:14 +0000

    Add dim & capacitance to rc
    So volume and thermal capacitance are now
    pulled from the BER columns

--- heat_demand.py
+++ heat_demand.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-import dask.dataframe as dd 
+import dask.dataframe as dd
+from icontract import require
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,11 +10,29 @@ import matplotlib
 
 from rc_buildingsimulator.building_physics import Zone  # Importing Zone Class
 from rc_buildingsimulator import supply_system
+from rc_buildingsimulator.supply_system import SupplySystemBase
+from rc_buildingsimulator.supply_system import SupplyOut
 from rc_buildingsimulator import emission_system
 from rc_buildingsimulator.radiation import Location
 from rc_buildingsimulator.radiation import Window
 
-matplotlib.style.use('ggplot')
+matplotlib.style.use("ggplot")
+
+COLUMNS = [
+    "WallArea",
+    "WindowArea",
+    "GroundFloorArea",
+    "GroundFloorHeight",
+    "FirstFloorHeight",
+    "SecondFloorHeight",
+    "ThirdFloorHeight",
+    "HSMainSystemEfficiency",
+    "UValueWall",
+    "UValueWindow",
+    "HeatExchangerEff",
+    "ThermalMassCategory",
+]
+
 
 def calculate_annual_household_heat_demand(
     City,
@@ -26,7 +45,9 @@ def calculate_annual_household_heat_demand(
 ):
     # A catch statement to prevent future coding bugs when modifying window area
     if window.area != zone.window_area:
-        raise ValueError('Window area defined in radiation file doesnt match area defined in zone')
+        raise ValueError(
+            "Window area defined in radiation file doesnt match area defined in zone"
+        )
 
     # Read Occupancy Profile
     occupancyProfile = pd.read_csv(path_to_occupancy_profile)
@@ -41,15 +62,13 @@ def calculate_annual_household_heat_demand(
     for hour in range(8760):
 
         # Occupancy for the time step
-        occupancy = occupancyProfile.loc[hour, 'People'] * max_occupancy
-        
+        occupancy = occupancyProfile.loc[hour, "People"] * max_occupancy
+
         # Gains from occupancy and appliances
-        internal_gains = (
-            occupancy * gain_per_person + appliance_gains * zone.floor_area
-        )
+        internal_gains = occupancy * gain_per_person + appliance_gains * zone.floor_area
 
         # Extract the outdoor temperature in City for that hour
-        t_out = City.weather_data['drybulb_C'][hour]
+        t_out = City.weather_data["drybulb_C"][hour]
 
         Altitude, Azimuth = City.calc_sun_position(
             latitude_deg=47.480,
@@ -60,8 +79,8 @@ def calculate_annual_household_heat_demand(
         window.calc_solar_gains(
             sun_altitude=Altitude,
             sun_azimuth=Azimuth,
-            normal_direct_radiation=City.weather_data['dirnorrad_Whm2'][hour],
-            horizontal_diffuse_radiation=City.weather_data['difhorrad_Whm2'][hour],
+            normal_direct_radiation=City.weather_data["dirnorrad_Whm2"][hour],
+            horizontal_diffuse_radiation=City.weather_data["difhorrad_Whm2"][hour],
         )
         zone.solve_energy(
             internal_gains=internal_gains,
@@ -74,45 +93,62 @@ def calculate_annual_household_heat_demand(
         t_m_prev = zone.t_m_next
 
         HeatingDemand.append(zone.heating_demand)
-    
+
     return sum(HeatingDemand)
 
 
-def calculate_annual_stock_heat_demand(
-    stock: pd.DataFrame = None
-) -> pd.DataFrame:
-    
+@require(
+    lambda stock: set(COLUMNS).issubset(set(stock.columns)),
+    "'stock' is missing required columns",
+)
+def calculate_annual_stock_heat_demand(stock: pd.DataFrame = None) -> pd.DataFrame:
+
     # Initialise the Location with a weather file
-    Dublin = Location(epwfile_path='IRL_Dublin.039690_IWEC.epw')
+    Dublin = Location(epwfile_path="IRL_Dublin.039690_IWEC.epw")
+
+    # Section 12.3.1.2 of ISO 13790
+    thermal_capacitance = {
+        "Low                 ": 80000,
+        "Medium-low          ": 110000,
+        "Medium              ": 165000,
+        "Medium-high         ": 260000,
+        "High                ": 370000,
+    }
 
     heat_demands = []
     for row in stock.itertuples():
+
+        total_internal_area = row.WallArea + row.WindowArea + row.GroundFloorArea
+        building_height = (
+            row.GroundFloorHeight
+            + row.FirstFloorHeight
+            + row.SecondFloorHeight
+            + row.ThirdFloorHeight
+        )
+        building_volume = building_height * row.GroundFloorArea
+        thermal_capacitance_per_floor_area = thermal_capacitance[
+            row.ThermalMassCategory
+        ]
+
         # Initialise an instance of the Zone. Empty spaces take on the default
         # parameters. See ZonePhysics.py to see the default values
         zone = Zone(
             window_area=row.WindowArea,
-            walls_area=11.0,
-            floor_area=row.WallArea,
-            room_vol=105,
-            total_internal_area=142.0,
-            lighting_load=11.7,
-            lighting_control=300.0,
-            lighting_utilisation_factor=0.45,
-            lighting_maintenance_factor=0.9,
+            walls_area=row.WallArea,
+            floor_area=row.GroundFloorArea,
+            room_vol=building_volume,
+            total_internal_area=total_internal_area,
             u_walls=row.UValueWall,
             u_windows=row.UValueWindow,
             ach_vent=1.5,
             ach_infl=0.5,
             ventilation_efficiency=row.HeatExchangerEff,
-            thermal_capacitance_per_floor_area=165000,
+            thermal_capacitance_per_floor_area=thermal_capacitance_per_floor_area,
             t_set_heating=20.0,
-            t_set_cooling=26.0,
             max_cooling_energy_per_floor_area=-np.inf,
             max_heating_energy_per_floor_area=np.inf,
             heating_supply_system=supply_system.OilBoilerMed,
-            cooling_supply_system=supply_system.HeatPumpAir,
             heating_emission_system=emission_system.NewRadiators,
-            cooling_emission_system=emission_system.AirConditioning,
         )
         # Define Windows
         south_window = Window(
@@ -129,9 +165,8 @@ def calculate_annual_stock_heat_demand(
             gain_per_person=100,  # W per person
             appliance_gains=14,  # W per sqm
             max_occupancy=3.0,
-            path_to_occupancy_profile='schedules_el_HOUSEHOLD.csv',
+            path_to_occupancy_profile="schedules_el_HOUSEHOLD.csv",
         )
         heat_demands.append(heat_demand)
-    
+
     return pd.Series(heat_demands)
-    
